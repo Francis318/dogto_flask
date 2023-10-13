cc45ce278acef038d21329f61bc27722b5564a2f
Francis318 <enriquereyes32@hotmail.com>
2023-10-13 17:28:18 -0600

    clase 3 terminada

--- dogtor/api/models.py
+++ dogtor/api/models.py
@@ -1,5 +1,5 @@
 from dogtor.db import db
-from sqlalchemy import Integer, String
+from sqlalchemy import Integer, String, DateTime
 from sqlalchemy.orm import mapped_column
 
 
@@ -7,3 +7,64 @@ class User(db.Model):
     id = mapped_column(Integer, primary_key=True)
     username = mapped_column(String, unique=True, nullable=False)
     email = mapped_column(String, unique=True)
+
+
+class Owner(db.Model):
+    """Pet owner object"""
+
+    id = db.Column(Integer, primary_key=True)
+    first_name = db.Column(String(length=50))
+    last_name = db.Column(String(length=50))
+    phone = db.Column(String(length=15))
+    mobile = db.Column(String(length=15))
+    email = db.Column(String)
+    pets = db.relationship("Pet")
+
+
+class Species(db.Model):
+    """Pet species object"""
+
+    id = db.Column(Integer, primary_key=True)
+    name = db.Column(String(length=20))
+    pet = db.relationship("Pet")
+
+
+class Pet(db.Model):
+    """Pet object"""
+
+    id = db.Column(Integer, primary_key=True)
+    name = db.Column(String(length=20))
+    owner_id = db.Column(Integer, db.ForeignKey("owner.id"))
+    age = db.Column(Integer)
+    species_id = db.Column(Integer, db.ForeignKey("species.id"))
+    record_id = db.Column(Integer, db.ForeignKey("record.id"))
+
+
+record_category_m2m = db.Table(
+    "record_category",
+    db.Column("record_id", Integer, db.ForeignKey("record.id")),
+    db.Column("category_id", Integer, db.ForeignKey("category.id")),
+)
+
+
+class Record(db.Model):
+    """Pet record object"""
+
+    id = db.Column(Integer, primary_key=True)
+    category = db.Column(String(length=20))
+    procedure = db.Column(String(length=255))
+    pet = db.relationship("Pet")
+    date = db.Column(DateTime)
+    categories = db.relationship(
+        "Category", secondary=record_category_m2m
+    )
+
+
+class Category(db.Model):
+    """Record category object"""
+
+    id = db.Column(Integer, primary_key=True)
+    name = db.Column(String(length=20))
+    records = db.relationship(
+        "Record", secondary=record_category_m2m
+    )
--- dogtor/api/views.py
+++ dogtor/api/views.py
@@ -1,5 +1,7 @@
-from . import api
 from flask import request
+from dogtor.db import db
+
+from . import api, models
 
 users_data = [
     {"id": 1, "username": "user0", "email": "user0@kodemia.mx"},
@@ -29,6 +31,7 @@ def users(user_id=None):
         return {"detail": f"user {data['username']} created"}
     return users_data
 
+
 @api.route("/pets/")
 def pets():
     return []
@@ -44,6 +47,38 @@ def procedures():
     return []
 
 
-@api.route("/species/")
-def species():
-    return []
+@api.route("/species/<int:species_id>", methods=["GET", "PUT", "DELETE"])
+@api.route("/species/", methods=["GET", "POST"])
+def species_endpoint(species_id=None):
+    try:
+        data = request.get_json()
+    except:
+        pass
+
+    if species_id is not None:
+        species = models.Species.query.get_or_404(species_id, "Species not found")
+        if request.method == "GET":
+            return {"id": species.id, "name": species.name}
+
+        if request.method == "PUT":
+            species.name = data["name"]
+            msg = f"species {species.name} modified"
+
+        if request.method == "DELETE":
+            db.session.delete(species)
+            msg = f"species {species.name} deleted"
+
+        db.session.commit()
+        return {"detail": msg}
+
+    if request.method == "GET":
+        species = models.Species.query.all()
+        return [{"id": species.id, "name": species.name} for species in species]
+
+    if request.method == "POST":
+        species = models.Species(name=data["name"])
+
+        db.session.add(species)
+        db.session.commit()
+
+        return {"detail": f"species {species.name} created successfully"}
